315fe02144ccfba489031d151fc7a854374e643c
santosh kumar sriramoju <santoshsriramoju2691@gmail.com>
2023-08-05 16:55:17 +0530

    Create test.js
    "test commit"

---
+++ test.js
@@ -0,0 +1,4 @@
+Clone the remote Git repo locally.
+Create a new branch with the branch, switch or checkout commands.
+Perform a git push with the –set-upstream option to set the remote repo for the new branch.
+Continue to perform Git commits locally on the new branch.
